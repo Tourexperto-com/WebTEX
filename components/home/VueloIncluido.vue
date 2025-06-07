f1a191ef169d6781b63d5bf655295ff74e393328
Lio <liobensignor@gmail.com>
2025-06-07 17:20:38 -0300

    Tours con vuelo incluido + Ventajas Banner

--- components/home/VueloIncluido.vue
+++ components/home/VueloIncluido.vue
@@ -1,5 +1,99 @@
 <template>
-    <DefaultSection class="h-32">
+    <DefaultSection>
         <HeadingH2>Tours con vuelo incluido</HeadingH2>
+        <CarouselStatic>
+            <HomeVueloIncluidoCard v-for="(producto, index) in productos" :key="index" :producto="producto"
+                class="w-48 flex-shrink-0 first-of-type:ml-4 last-of-type:mr-4" />
+        </CarouselStatic>
+        <ButtonPrimary>Ver más tours con aéreos</ButtonPrimary>
     </DefaultSection>
 </template>
+
+
+<script setup>
+import { HomeVueloIncluidoCard } from '#components';
+
+const productos = [
+    {
+        url: '#',
+        titulo: 'Madrid, París, Alpes e Italia "clásico"',
+        img: '/images/product/product.jpg',
+        sticker: '/images/product/sticker.png',
+        duracion: '13 días/12 noches',
+        iniciaFinaliza: 'Berlín/Viena',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "2.000",
+        precioTachado: "2.200",
+        calificacion: "Excelente"
+    },
+    {
+        url: '#',
+        titulo: 'Londres, Amsterdam y Bruselas',
+        img: '/images/product/product.jpg',
+        sticker: '/images/product/sticker.png',
+        duracion: '8 días/7 noches',
+        iniciaFinaliza: 'Ciudad del Cabo/Ciudad del Cabo',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "1.500",
+        calificacion: "Muy bueno"
+    },
+    {
+        url: '#',
+        titulo: 'Circuito por Escandinavia',
+        img: '/images/product/product.jpg',
+        duracion: '15 días/14 noches',
+        iniciaFinaliza: 'Estocolmo/Oslo',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "2.800",
+        precioTachado: "3.200",
+        calificacion: "Excelente"
+    },
+    {
+        url: '#',
+        titulo: 'Barcelona, Niza y Roma Express',
+        img: '/images/product/product.jpg',
+        duracion: '10 días/9 noches',
+        iniciaFinaliza: 'Barcelona/Roma',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "1.800",
+        precioTachado: "2.100",
+        calificacion: "Bueno"
+    },
+    {
+        url: '#',
+        titulo: 'Praga, Viena y Budapest Imperial',
+        img: '/images/product/product.jpg',
+        sticker: '/images/product/sticker.png',
+        duracion: '12 días/11 noches',
+        iniciaFinaliza: 'Praga/Budapest',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "2.300",
+        precioTachado: "2.600",
+        calificacion: "Excelente"
+    },
+    {
+        url: '#',
+        titulo: 'Grecia Clásica y Islas',
+        img: '/images/product/product.jpg',
+        sticker: '/images/product/sticker.png',
+        duracion: '14 días/13 noches',
+        iniciaFinaliza: 'Atenas/Santorini',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "2.500",
+        precioTachado: "2.900",
+        calificacion: "Muy bueno"
+    },
+    {
+        url: '#',
+        titulo: 'Suiza y Alemania Romántica',
+        img: '/images/product/product.jpg',
+        sticker: '/images/product/sticker.png',
+        duracion: '11 días/10 noches',
+        iniciaFinaliza: 'Zurich/Munich',
+        salidas: "Agosto a Septiembre 2025",
+        precio: "2.200",
+        precioTachado: "2.500",
+        calificacion: "Excelente"
+    }
+];
+</script>
---
+++ components/ventaja/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <CarouselStatic>
+            <NuxtImg v-for="(item, index) in banners" :key="index" :src="item.img" :alt="item.alt" class="h-52 rounded-[18px] shadow-1 first-of-type:ml-4 last-of-type:mr-4" />
+        </CarouselStatic>
+    </section>
+</template>
+
+<script setup>
+const banners = [
+    {
+        img: "/images/ventajas/banner-a.png",
+        alt: "",
+    },
+    {
+        img: "/images/ventajas/banner-b.png",
+        alt: "",
+    },
+    {
+        img: "/images/ventajas/banner-c.png",
+        alt: "",
+    },
+];
+</script>
