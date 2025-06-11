07b3c599d2b161088adcfa59dbb1b551bd20f9a8
Emilio Bensignor <liobensignor@gmail.com>
2025-06-10 21:28:43 -0300

    Opiniones tripadvisor

--- components/home/OpinionTripadvisor.vue
+++ components/home/OpinionTripadvisor.vue
@@ -1,5 +1,5 @@
 <template>
-    <DefaultSection class="px-4">
+    <DefaultSection class="px-4 lg:px-0">
         <TripadvisorOpiniones :url="'https://www.tripadvisor.es/Attraction_Review-g34152-d26305486-Reviews-Tour_Experto-Coral_Gables_Florida.html'" />
     </DefaultSection>
 </template>
--- components/tripadvisor/Opiniones.vue
+++ components/tripadvisor/Opiniones.vue
@@ -1,18 +1,18 @@
 <template>
-    <NuxtLink :to="url" class="flex flex-col lg:flex-row lg:bg-tripadvisor-green border-2 lg:border-none border-tripadvisor-green rounded-[20px] lg:rounded-full" target="_blank">
+    <NuxtLink :to="url" class="lg:w-full lg:max-w-[750px] lg:h-14 flex flex-col lg:flex-row lg:items-center lg:justify-between lg:bg-tripadvisor-green border-2 lg:border-none border-tripadvisor-green rounded-[20px] lg:rounded-full lg:p-1 lg:pl-8" target="_blank">
         <div class="bg-tripadvisor-green lg:bg-none rounded-t-[18px] lg:rounded-none py-2">
-            <p class="text-center font-medium">Opiniones de nuestros viajeros</p>
+            <p class="text-center lg:text-xl font-semibold">Opiniones de nuestros viajeros</p>
         </div>
-        <div class="flex justify-center items-center gap-2 lg:bg-light rounded-b-[18px] lg:rounded-none py-2 px-6">
-            <p class="text-sm font-bold font-roboto">4.5</p>
+        <div class="lg:h-full flex justify-center items-center gap-2 lg:bg-light rounded-b-[18px] lg:rounded-full py-2 px-6">
+            <p class="text-sm lg:text-xl font-bold font-roboto">4.5</p>
             <div class="flex items-center gap-1">
-                <div v-for="n in 4" :key="n" class="w-4 h-4 bg-tripadvisor-green rounded-full"></div>
-                <div class="w-4 h-4 border border-tripadvisor-green rounded-full relative overflow-hidden">
+                <div v-for="n in 4" :key="n" class="w-4 h-4 lg:w-5 lg:h-5 bg-tripadvisor-green rounded-full"></div>
+                <div class="w-4 h-4 lg:w-5 lg:h-5 border border-tripadvisor-green rounded-full relative overflow-hidden">
                     <span class="absolute left-0 top-0 h-full w-1/2 bg-tripadvisor-green"></span>
                     <span class="absolute right-0 top-0 h-full w-1/2"></span>
                 </div>
             </div>
-            <NuxtImg src="/images/tripadvisor/Logo-Tripadvisor.svg" alt="Logo-Tripadvisor" class="h-4" />
+            <NuxtImg src="/images/tripadvisor/Logo-Tripadvisor.svg" alt="Logo-Tripadvisor" class="h-4 lg:h-6" />
         </div>
     </NuxtLink>
 </template>
